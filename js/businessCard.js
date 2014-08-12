f16e478e94159179ef480cca7169afe2035d5c4a
Andrew McDowell <madoliole@gmail.com>
2014-08-12 15:56:09 +1000

    refactor

--- js/businessCard.js
+++ js/businessCard.js
@@ -2,14 +2,33 @@
 
 var dataSource = {
     name: 'Andrew McDowell',
-    tagline: 'Javascript nutter with a side of grumpy cat',
-    github: 'http://github.com/madole',
-    twitter: 'http://twitter.com/madole',
-    facebook: 'http://facebook.com/madole404',
-    linkedIn: 'http://linkedin.com/andrew.mcdowell',
-    email: 'mailto:madoliole@gmail.com',
+    tagline: 'Javascript engineer with a side of grumpy cat',
+    icons: [
+        {github: 'http://github.com/madole'},
+        {twitter: 'http://twitter.com/madole'},
+        {facebook: 'http://facebook.com/madole404'},
+        {linkedin: 'http://linkedin.com/andrew.mcdowell'},
+        {envelope: 'mailto:madoliole@gmail.com'}
+    ],
     gravitar: 'https://pbs.twimg.com/profile_images/378800000342584802/a3c1efafd175e220231ef582549abec5_400x400.jpeg'
 };
+
+var iconsList = React.createClass({
+    render: function(){
+      var icons = this.props.data.icons;
+      return (
+          <ul>
+              {icons.map(function(icon){
+                  var key = Object.keys(icon)[0] ;
+                  var keyClass = 'fa-'+Object.keys(icon)[0]+'' ;
+                  var classes = "fa " + keyClass + " fa-3x buzz";
+                  var val = '"' +icon[key] +'"';
+                  return <li ><a href= {val} ><i className={classes}></i></a></li>;
+              })}
+          </ul>
+      );
+    }
+});
 
 var BusinessCard = React.createClass({
     render: function() {
@@ -21,13 +40,7 @@ var BusinessCard = React.createClass({
                 <img className="meHead" src={this.props.data.gravitar}></img>
                 <blockquote>{this.props.data.tagline}</blockquote>
                 <div className = "socialMedia">
-                    <ul>
-                        <li><a href = {this.props.data.github}><i className="icons buzz fa fa-github fa-3x"></i></a></li>
-                        <li><a href = {this.props.data.twitter}><i className="icons buzz fa fa-twitter fa-3x"></i></a></li>
-                        <li><a href = {this.props.data.facebook}><i className="icons buzz fa fa-facebook fa-3x"></i></a></li>
-                        <li><a href = {this.props.data.linkedIn}><i className="icons buzz fa fa-linkedin fa-3x"></i></a></li>
-                        <li><a href = {this.props.data.email}><i className="icons buzz fa fa-envelope fa-3x"></i></a></li>
-                    </ul>
+                    <iconsList data = {this.props.data}/>
                 </div>
             </div>
             );
